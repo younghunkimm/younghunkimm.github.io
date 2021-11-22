091ffa66c1b5b72512467b427b3a6dbded1159af
김영훈 <91935220+younghunkimm@users.noreply.github.com>
2021-11-22 20:55:46 +0900

    Add files via upload

---
+++ pf03/js/pf03.js
@@ -0,0 +1,93 @@
+$(function(){
+// -----------------------------------
+
+$('#main_player').YTPlayer({
+    videoURL:'https://www.youtube.com/watch?v=wq0iC472QeQ',
+    containment:'#main_visual',
+    autoPlay:true,
+    mute:true,
+    startAt:9,
+    opacity:1,
+    showControls:false,
+    pauseOnFocus:false,
+    pauesOnHover:false,
+});
+
+$('.pr_slider').slick({
+    slidesToShow:1,
+    arrows:false,
+    fade:true,
+    asNavFor:'.txt_slider',
+});
+
+$('.txt_slider').slick({
+    slidesToShow:1,
+    arrows:false,
+    fade:true,
+    asNavFor:'.pr_slider',
+});
+
+$('.pr_slider').on('afterChange', function(e,s,c){
+    $('.category>ul>li').eq(c).addClass('on').siblings().removeClass('on');
+});
+
+$('.category>ul>li').eq(0).addClass('on');
+$('.category>ul>li').on('click', function(){
+    var idx= $(this).index();
+    // console.log(idx);
+    $('.pr_slider').slick('slickGoTo', idx);
+    $('.txt_slider').slick('slickGoTo', idx);
+    $(this).toggleClass('on').siblings().removeClass('on');
+});
+
+$('.mv01').YTPlayer({
+    videoURL:'https://www.youtube.com/watch?v=xbzdHIYyJ4Y',
+    containment:'.main_case',
+    autoPlay:true,
+    mute:true,
+    startAt:0,
+    opacity:1,
+    showControls:false,
+    playOnlyIfVisible:true,
+    optimizeDisplay:false,
+    pauseOnFocus:true,
+    loop:true,
+})
+
+$('.insta_slider').slick({
+    autoplay:true,
+    autoplaySpeed:2000,
+    centerMode:true,
+    centerPadding:'400px',
+    slidesToShow:6,
+    arrows:false,
+    swipeToSlide:true,
+    responsive: [
+        {
+            breakpoint:768,
+            settings: {
+                slidesToShow: 3,
+                centerPadding:'50px',
+            }
+        }
+    ]
+});
+
+$('.notice_slider').slick({
+    autoplay:true,
+    arrows:false,
+    vertical:true,
+})
+
+$('.up_case').on('click', function(){
+    $('.notice_slider').slick('slickPrev')
+})
+$('.down_case').on('click', function(){
+    $('.notice_slider').slick('slickNext')
+})
+
+$('#con05 input').on('click', function(){
+    $(this).empty();
+})
+// -----------------------------------
+});
