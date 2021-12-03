220335686a404169d5684c9390dc50ff2b850218
김영훈 <91935220+younghunkimm@users.noreply.github.com>
2021-12-03 12:01:47 +0900

    Add files via upload

--- xeicode/js/xeicode.js
+++ xeicode/js/xeicode.js
@@ -10,6 +10,10 @@ $('.category').on('click', function(){
     $('html, body').animate({scrollTop:0}, 0);
 });
 
+$('.toTop').on('click', function(){
+    $('html, body').animate({scrollTop:0}, 0);
+});
+
 // -----------------------
 
 });
