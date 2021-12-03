391dfbe8b5bcd7505b95d3555c658552e87afd6e
김영훈 <91935220+younghunkimm@users.noreply.github.com>
2021-12-03 12:04:27 +0900

    Add files via upload

---
+++ doc/vertical02.js
@@ -0,0 +1,31 @@
+$(function(){
+// -------------------------------------------------------
+$('.main_visual').slick({
+    // arrows:false,
+    autoplay:true,
+    dots:true,
+    pauseOnHover:false,
+    pauseOnFocus:false,
+    prevArrow:'<div class="prev"><i class="xi-arrow-left"></i></div>',
+    nextArrow:'<div class="next"><i class="xi-arrow-right"></i></div>',
+});
+
+$('.main_visual figure').eq(1).addClass('on')
+$('.main_visual').on('afterChange', function(e,s,c){
+    $('.main_visual figure').eq(c+1).addClass('on').siblings().removeClass('on');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+// -------------------------------------------------------
+});
