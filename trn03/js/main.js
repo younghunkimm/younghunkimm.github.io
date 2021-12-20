447a7a6e93e673426e3148da8588183271f5df66
김영훈 <91935220+younghunkimm@users.noreply.github.com>
2021-12-20 15:06:53 +0900

    Add files via upload

---
+++ trn03/js/main.js
@@ -0,0 +1,23 @@
+const HD = document.querySelector('#hd');
+window.addEventListener('scroll', ()=>{
+    let sct = window.scrollY;
+    sct > 0 ? HD.classList.add('on') : HD.classList.remove('on')
+});
+
+const mainSlider = new Swiper('.main_slider', {
+    loop:true,
+    slidesPerView: 2,
+    spaceBetween: 150,
+    autoplay: {
+        delay:2000,
+    },
+    pagination: {
+        el: '.main_visual .page',
+        type: "fraction",
+    },
+    navigation: {
+        nextEl: '.main_visual .btn_next',
+        prevEl: '.main_visual .btn_prev',
+    },
+    autopalyDisableOnInteraction:false,
+});
