633ae9bbac2bb47d01e733c3d2c6ee076be224fb
김영훈 <91935220+younghunkimm@users.noreply.github.com>
2021-12-21 14:08:15 +0900

    Add files via upload

--- trn03/js/main.js
+++ trn03/js/main.js
@@ -4,6 +4,7 @@ window.addEventListener('scroll', ()=>{
     sct > 0 ? HD.classList.add('on') : HD.classList.remove('on')
 });
 
+const slideBar = document.querySelector('.slide_bottom .bar');
 const mainSlider = new Swiper('.main_slider', {
     loop:true,
     slidesPerView: 2,
@@ -21,3 +22,47 @@ const mainSlider = new Swiper('.main_slider', {
     },
     autopalyDisableOnInteraction:false,
 });
+
+slideBar.classList.add('on');
+mainSlider.on('slideChangeTransitionEnd', ()=> {
+    slideBar.classList.add('on');
+});
+mainSlider.on('slideChangeTransitionStart', ()=> {
+    slideBar.classList.remove('on');
+});
+
+const productSlider = new Swiper('.pr_slider', {
+    loop:false,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+        el: '.pr_btn .pr_dots',
+    },
+    navigation: {
+        nextEl: '.pr_btn .btn_next',
+        prevEl: '.pr_btn .btn_prev',
+    },
+});
+
+//scroll event
+const SEC = document.querySelectorAll('.action');
+const WT=window.innerHeight;
+
+window.addEventListener("scroll", ()=>{
+        let sct=window.scrollY;
+        SEC.forEach(e=>{
+            let secTop = e.offsetTop;
+            let secH=e.clientHeight;
+            if (sct > secTop - (WT-secH) / 2 - 200) {
+                e.classList.add('on');
+            };
+            // sct > secTop - (WT-secH) / 2 - 200 ? e.classList.add('on') : e.classList.remove('on');
+        });
+});
+
+
+
+
+
+
+
