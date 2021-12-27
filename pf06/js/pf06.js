7cf07ff6c55f2bec6daae77b912971062745d017
김영훈 <91935220+younghunkimm@users.noreply.github.com>
2021-12-27 19:51:40 +0900

    Add files via upload

--- pf06/js/pf06.js
+++ pf06/js/pf06.js
@@ -111,22 +111,28 @@ $(window).on('scroll', function(){
 });
 
 $('.mopen').on('click', function(){
-    $('.gnb').toggleClass('on');
-    $('.hd_top').toggleClass('on');
-    $(this).toggleClass('active');
-    $('.login').toggleClass('on');
-    $('html, body').toggleClass('no_scroll');
+    if ($(window).width() <= 768) {
+        $('.gnb').toggleClass('on');
+        $('.hd_top').toggleClass('on');
+        $(this).toggleClass('active');
+        $('.login').toggleClass('on');
+        $('html, body').toggleClass('no_scroll');
+    };
 });
 
 $('.depth01>li>a').on('click', function(){
-    $(this).next().stop().slideToggle();
-    $(this).parent().siblings().find('.depth02').stop().slideUp();
+    if ($(window).width() <= 768) {
+        $(this).next().stop().slideToggle();
+        $(this).parent().siblings().find('.depth02').stop().slideUp();
+    };
 });
 
 $('.depth02 dt').on('click', function(){
-    $(this).parent().find('dd').slideToggle();
-    $(this).parent().siblings().find('dd').stop().slideUp();
-    $(this).parent().parent().siblings().find('dd').stop().slideUp();
+    if ($(window).width() <= 768) {
+        $(this).parent().find('dd').slideToggle();
+        $(this).parent().siblings().find('dd').stop().slideUp();
+        $(this).parent().parent().siblings().find('dd').stop().slideUp();
+    };
 });
 
 $(window).on('resize', function(){
