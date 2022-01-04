c79087aadedb88f01e2e66cca80bf1a88fb6cf4a
김영훈 <91935220+younghunkimm@users.noreply.github.com>
2022-01-04 18:43:54 +0900

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,45 @@
+$(function(){
+// --------------------------------------------
+
+let sc = $('.section');
+let sideBar = $('nav li')
+$('#main').fullpage({
+    anchors:['intro', 'portfolio_01', 'portfolio_02', 'portfolio_03', 'portfolio_04', 'portfolio_05', 'profile'],
+    afterLoad: function(origin, destination, direction){
+        let idx = destination.index;
+        sc.eq(idx).addClass('on').siblings().removeClass('on');
+        sideBar.eq(idx).addClass('on').siblings().removeClass('on');
+    },
+});
+
+let typed01 = new Typed('.typed01', {
+    strings: ['WEB Is My MONEY'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    fadeOut: true,
+    cursorChar: 'ㅣ',
+    loop: false,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// --------------------------------------------
+});
