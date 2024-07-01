ca38b5132b2f82b4084981c96c07e327b3d8eaa4
김영훈 <hunifive@naver.com>
2024-07-01 17:26:45 +0900

    스크롤 게이지 추가

--- _javascript/modules/layouts/basic.js
+++ _javascript/modules/layouts/basic.js
@@ -2,6 +2,6 @@ import { back2top } from '../components/back-to-top';
 import { loadTooptip } from '../components/tooltip-loader';
 
 export function basic() {
-  back2top();
-  loadTooptip();
+    back2top();
+    loadTooptip();
 }
